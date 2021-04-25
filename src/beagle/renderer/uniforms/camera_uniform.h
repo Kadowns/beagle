806bcaae6369d6f0a55dca958fcd441838367e7a
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-04-25 17:05:09 -0300

    -Add camera uniform

---
+++ src/beagle/renderer/uniforms/camera_uniform.h
@@ -0,0 +1,19 @@
+//
+// Created by Ricardo on 4/25/2021.
+//
+
+#ifndef BEAGLE_CAMERA_UNIFORM_H
+#define BEAGLE_CAMERA_UNIFORM_H
+
+#include <beagle/math.h>
+
+namespace beagle {
+
+struct CameraUniform {
+    glm::mat4 vp;
+};
+
+
+}
+
+#endif //BEAGLE_CAMERA_UNIFORM_H
